6e83de10e98392bf06d189d4c2a75e8ce39b5312
Vincent Lo <vincentsylo@gmail.com>
2016-02-22 15:17:12 +1100

    Tweaks to scrollbar and cleaning up

--- test/components/views/CreateListViewComponentTest.js
+++ test/components/views/CreateListViewComponentTest.js
@@ -7,16 +7,16 @@
 // import TestUtils from 'react-addons-test-utils';
 import createComponent from 'helpers/shallowRenderHelper';
 
-import MyListsViewComponentComponent from 'components/MyListsViewComponent.js';
+import CreateListViewComponent from 'components/views/CreateListViewComponent.js';
 
-describe('MyListsViewComponent', () => {
+describe('CreateListViewComponent', () => {
   let component;
 
   beforeEach(() => {
-    component = createComponent(MyListsViewComponentComponent);
+    component = createComponent(CreateListViewComponent);
   });
 
   it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('mylistsview-component');
+    expect(component.props.className).to.equal('createlistview-component');
   });
 });
