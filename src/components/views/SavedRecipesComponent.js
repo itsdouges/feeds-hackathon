119ecac818d8a2788b8aaa5badf7159e7271f775
Vincent Lo <vincentsylo@gmail.com>
2016-02-22 15:09:16 +1100

    Set up random recipe feed

--- src/components/views/SavedRecipesComponent.js
+++ src/components/views/SavedRecipesComponent.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { mapStateToProps, mapDispatchToProps } from '../../reducers/mapping';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 import RecipeList from './../RecipeListComponent';
 
 import '../../styles/views/SavedRecipes.less';
@@ -11,6 +11,27 @@ import '../../styles/views/SavedRecipes.less';
 class SavedRecipesComponent extends React.Component {
   constructor(props) {
     super(props);
+
+    this.random = this.random.bind(this);
+  }
+
+  random(e) {
+    e.preventDefault();
+
+    const localRecipes = this.props.state.recipe.localRecipes;
+    const onlineRecipes = this.props.state.recipe.onlineRecipes;
+    const allRecipes = {
+      ...localRecipes,
+      ...onlineRecipes
+    };
+    let randomNumber = Math.floor(Math.random() * Object.keys(allRecipes).length);
+    let randomRecipeKey = Object.keys(allRecipes)[randomNumber];
+
+    if (localRecipes[randomRecipeKey]) {
+      browserHistory.push('/recipe/view/local/' + randomRecipeKey);
+    } else if (onlineRecipes[randomRecipeKey]) {
+      browserHistory.push('/recipe/view/online/' + randomRecipeKey);
+    }
   }
 
   render() {
@@ -18,12 +39,17 @@ class SavedRecipesComponent extends React.Component {
     const onlineRecipes = this.props.state.recipe.onlineRecipes;
 
     const exists = (Object.keys(localRecipes).length || Object.keys(onlineRecipes).length);
-
     const message = !exists && <span className="recipe-description">Oh, you have no recipes.  Go <Link className="btn-link" to="/recipe/find">find</Link> and <Link className="btn-link" to="/recipe/create">create</Link> some :-)</span>;
 
     return (
       <div className="page">
         <div className="savedrecipes-component">
+          <div className="home-actions">
+            <a href="#" onClick={this.random}>
+              <i style={{marginRight:'0.25em'}} className="fa fa-random"></i> random <span className="feed">feed</span>
+            </a>
+          </div>
+
           <RecipeList recipes={localRecipes} isLocal />
           <RecipeList recipes={onlineRecipes} />
           {message}
